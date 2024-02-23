e3b966f4f9efed940c402981cf4c7b8d5984fc9c
aliciamb86 <alicia.mb86@gmail.com>
2024-02-23 19:56:37 +0100

    23/02/24 19:56

--- Alicia/.py/funciones.py
+++ Alicia/.py/funciones.py
@@ -2,16 +2,16 @@ import numpy as np
 import random
 import time
 
-def crear_tablero_jug(tamaño=(10,10)):
-    tablero_jug = np.full(tamaño, "·")
-    return tablero_jug
+def crear_tablero(tamaño=(10,10)):
+    tablero = np.full(tamaño, "·")
+    return tablero
 
-def colocar_barco_jug(barco, tablero_jug):
+def colocar_barco(barco, tablero):
     for casilla in barco:
-        tablero_jug[casilla] = "O"
-    return tablero_jug
+        tablero[casilla] = "O"
+    return tablero
 
-def crear_barco_jug(eslora, tablero_jug, contador):    
+def crear_barco(eslora, tablero, contador):    
     # elegimos aleatoriamente el punto de partida desde el que se colocará nuestro barco
     fila_a = random.randint(0,9)
     columna_a = random.randint(0,9)
@@ -24,7 +24,7 @@ def crear_barco_jug(eslora, tablero_jug, contador):
 
     # elegimos aleatoriamente la dirección en la que se posicionará nuestro barco
     orientacion = random.choice(["S","O","E","N"])
- 
+
     while len(barco) < eslora:
         match orientacion:
             case "O":
@@ -32,34 +32,44 @@ def crear_barco_jug(eslora, tablero_jug, contador):
                     columna_inicio -= 1 
                 else:
                     orientacion = random.choice(["S","E","N"])
+
                     continue
             case "E":
                 if columna_a + (eslora - 1) <= 9:
                     columna_inicio += 1
                 else:
                     orientacion = random.choice(["S","O","N"])
+
                     continue
             case "S":
                 if fila_a + (eslora - 1) <= 9:
                     fila_inicio += 1
                 else:
                     orientacion = random.choice(["O","E","N"])
+
                     continue
             case "N":
                 if fila_a - (eslora - 1) >= 0:
                     fila_inicio -= 1
                 else:
                     orientacion = random.choice(["S","O","E"])
+
                     continue
         barco.append((fila_inicio, columna_inicio))
     else:
         for casilla in barco:
-            if tablero_jug[casilla] != "O":
+            if tablero[casilla] != "O":
                 contador +=1
                 if contador == eslora:
                     # barcos.append(barco)
-                    tablero_jug = colocar_barco_jug(barco, tablero_jug)
+                    tablero = colocar_barco(barco, tablero)
+
                     break
             else:
-                crear_barco(eslora, tablero_jug, contador)
+                # print('Creando nuevo barco')
+                crear_barco(eslora, tablero, contador)
                 break
+
+def ocultar_tablero(tablero):
+    tablero[::,::] = '·'
+    return tablero
--- Alicia/.py/main.py
+++ Alicia/.py/main.py
@@ -4,17 +4,41 @@ import time
 import variables as v
 import funciones as f
 
-tablero_jug = f.crear_tablero_jug()
-print(tablero_jug)
+# creamos el tablero del jugador 1
+tablero_jug = f.crear_tablero()
+# print(tablero_jug)
 
-barco_1_1 = f.crear_barco_jug(1, tablero_jug, v.contador)
-barco_2_1 = f.crear_barco_jug(1, tablero_jug, v.contador)
-barco_3_1 = f.crear_barco_jug(1, tablero_jug, v.contador)
-barco_4_1 = f.crear_barco_jug(1, tablero_jug, v.contador)
-barco_5_1 = f.crear_barco_jug(2, tablero_jug, v.contador)
-barco_6_1 = f.crear_barco_jug(2, tablero_jug, v.contador)
-barco_7_1 = f.crear_barco_jug(2, tablero_jug, v.contador)
-barco_8_1 = f.crear_barco_jug(3, tablero_jug, v.contador)
-barco_9_1 = f.crear_barco_jug(3, tablero_jug, v.contador)
-barco_10_1 = f.crear_barco_jug(4, tablero_jug, v.contador)
-print(tablero_jug)
+# creamos los barcos del jugador 1
+barco_1j = f.crear_barco(1, tablero_jug, v.contador)
+barco_2j = f.crear_barco(1, tablero_jug, v.contador)
+barco_3j = f.crear_barco(1, tablero_jug, v.contador)
+barco_4j = f.crear_barco(1, tablero_jug, v.contador)
+barco_5j = f.crear_barco(2, tablero_jug, v.contador)
+barco_6j = f.crear_barco(2, tablero_jug, v.contador)
+barco_7j = f.crear_barco(2, tablero_jug, v.contador)
+barco_8j = f.crear_barco(3, tablero_jug, v.contador)
+barco_9j = f.crear_barco(3, tablero_jug, v.contador)
+barco_10j = f.crear_barco(4, tablero_jug, v.contador)
+print('TABLERO JUGADOR \n', tablero_jug)
+
+
+# creamos el tablero de la máquina
+tablero_maq = f.crear_tablero()
+# print(tablero_maq)
+
+# creamos los barcos de la máquina
+barco_1m = f.crear_barco(1, tablero_maq, v.contador)
+barco_2m = f.crear_barco(1, tablero_maq, v.contador)
+barco_3m = f.crear_barco(1, tablero_maq, v.contador)
+barco_4m = f.crear_barco(1, tablero_maq, v.contador)
+barco_5m = f.crear_barco(2, tablero_maq, v.contador)
+barco_6m = f.crear_barco(2, tablero_maq, v.contador)
+barco_7m = f.crear_barco(2, tablero_maq, v.contador)
+barco_8m = f.crear_barco(3, tablero_maq, v.contador)
+barco_9m = f.crear_barco(3, tablero_maq, v.contador)
+barco_10m = f.crear_barco(4, tablero_maq, v.contador)
+# print(tablero_maq)
+
+# ocultamos el tablero de la máquina
+tablero_maq_oculto = f.ocultar_tablero(tablero_maq)
+print('TABLERO MÁQUINA \n', tablero_maq_oculto)
